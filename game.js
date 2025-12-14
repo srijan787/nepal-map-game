78826ff48a56409b8a7b2c9d26bb8f25567c8803
srijan787 <dsrijan787@gmail.com>
2025-12-14 16:06:27 +0545

    made js changes

--- game.js
+++ game.js
@@ -1,15 +1,49 @@
 let districts = document.querySelectorAll(".district");
 let currentAnswer = "";
+let score = 0;
+let districtQueue =[];
 
 function startGame() {
     score=0;
+    districts.forEach(district => district.classList.remove("correct","wrong"));
+
+    shuffled();
     nextQuestion();
-    alert("The game has started! Guess the district.");
+}
+
+function shuffled(){
+    districtQueue=Array.from(districts);
+
+    for(let i=districtQueue.length-1;i>0;i--){
+        let j= Math.floor(Math.random() * (i+1));
+        [districtQueue[i],districtQueue[j]] = [districtQueue[j],districtQueue[i]];
+    }
 }
 
 function nextQuestion() {
-    let randomIndex = Math.floor(Math.random() * districts.lenght);
-    currentAnswer = districts[randomIndex].id;
+    let nextDistrict=districtQueue.pop();
+    currentAnswer=nextDistrict.id;
 
-    console.log("answer:", currentAnswer);
+    document.getElementById("question").textContent=
+    "Find this :"+ currentAnswer;
+
+    if(districtQueue.length===0){
+    document.getElementById("score").textContent=
+    "Final Score:"+score;
+    return;
+    }
 }
+districts.forEach(district => {
+
+    district.addEventListener("click",() =>{
+
+        if(district.id === currentAnswer){
+            district.classList.add("correct");
+            score++;
+        }
+        else{
+            document.getElementById(currentAnswer).classList.add("wrong");
+        }
+        nextQuestion();
+    });
+});
